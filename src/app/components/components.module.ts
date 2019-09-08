d6e497b3a585f3a2a24cfe61e488f9ba1b4e9380
Absner Anaya <absrraniel@gmail.com>
2019-09-08 10:14:15 -0400

    creando component card producto

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CardProductComponent } from './card-product/card-product.component';
 
 const COMPONENTS: Array<any>  = [
-  NavbarComponent
+  NavbarComponent,
+  CardProductComponent
 ];
 
 @NgModule({
