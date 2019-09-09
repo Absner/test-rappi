99ddc611cb9b5fe8481ecd1202e7e79b48ba89b4
Absner Anaya <absrraniel@gmail.com>
2019-09-08 21:45:37 -0400

    store

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -5,16 +5,24 @@ import {
   createSelector,
   MetaReducer
 } from '@ngrx/store';
+import { localStorageSync } from 'ngrx-store-localstorage';
 import { environment } from '../../environments/environment';
+
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({
+    keys: ['shoppingCar'],
+    rehydrate: true
+  })(reducer);
+}
 
 
 export interface State {
-
+  shoppingCar: any;
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  shoppingCar: null
 };
 
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [localStorageSyncReducer] : [localStorageSyncReducer];
